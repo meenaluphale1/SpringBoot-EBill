277258bc15124c0eedc885910ae6adc51414bd79
MeenalAnand <meenaluphalegit@gmail.com>
2022-03-05 23:58:44 +0530

    first commit

---
+++ SpringBoot-EBill/src/main/java/com/lti/prasanna/SpringBootEBillApplication.java
@@ -0,0 +1,13 @@
+package com.lti.prasanna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEBillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEBillApplication.class, args);
+	}
+
+}
