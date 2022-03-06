b00c3c0500c6d686fac5b673cf1446c68ceb5be3
MeenalAnand <meenaluphalegit@gmail.com>
2022-03-06 10:17:54 +0530

    added serveltinitializer

---
+++ SpringBoot-EBill/src/main/java/com/lti/prasanna/ServletInitializer.java
@@ -0,0 +1,15 @@
+package com.lti.prasanna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+	   @Override
+	   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	      return application.sources(SpringBootEBillApplication.class);
+	   }
+	   public static void main(String[] args) {
+	      SpringApplication.run(SpringBootEBillApplication.class, args);
+	   }
+	}
