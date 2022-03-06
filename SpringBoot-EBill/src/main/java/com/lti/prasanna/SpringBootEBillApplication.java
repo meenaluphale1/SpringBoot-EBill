2a10313a5b4988008441b552738fdf82dec2d4ed
MeenalAnand <meenaluphalegit@gmail.com>
2022-03-06 11:33:19 +0530

    changes made in main app

--- SpringBoot-EBill/src/main/java/com/lti/prasanna/SpringBootEBillApplication.java
+++ SpringBoot-EBill/src/main/java/com/lti/prasanna/SpringBootEBillApplication.java
@@ -2,13 +2,18 @@ package com.lti.prasanna;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootEBillApplication {
+public class SpringBootEBillApplication extends SpringBootServletInitializer{
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootEBillApplication.class, args);
-		
-	}
-
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootEBillApplication.class);
+    }
+ 
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootEBillApplication.class, args);
+    }
 }
